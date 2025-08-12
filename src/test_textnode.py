fa583af7c1399e77dcad31b73de4a6d15517e746
Nodesz-Dev <harleykempita@gmail.com>
2025-08-12 15:33:51 +1000

    Added text_node_to_html_node() to the textnode.py. This converts a text node to a leafnode. added multiple test cases to test the functionality of this function to the test_textnode.py

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -20,6 +20,26 @@ class TestTextNode(unittest.TestCase):
 
         node6 = TextNode("This is a text node ", TextType.CODE, "https")
         self.assertEqual(node5, node6)
+
+class TestTestNodeToHTML(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold node")
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "www.image.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, None)
+        self.assertEqual(html_node.props, {"src": "www.image.com", "alt": "This is an image"})
 
 
 if __name__ == "__main__":
