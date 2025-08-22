93e397ba5e2c8effa4c4ec563c70e1408b6b98fd
Nodesz-Dev <harleykempita@gmail.com>
2025-08-22 18:44:58 +1000

    Added in the block_to_block_type function in the block_markdown.py file along with the BlockType enum class. added many test cases for this function to the test_block_markdown.py

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,6 +1,14 @@
 from textnode import *
 import re
 
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     split_blocks = markdown.split("\n\n")
     full_blocks = []
@@ -11,3 +19,31 @@ def markdown_to_blocks(markdown):
         full_blocks.append(stripped_block)
 
     return full_blocks
+
+def block_to_block_type(block):
+    split_lines = block.split("\n")
+    
+    if block.startswith(("#","##", "###", "####", "#####", "######")):
+        return BlockType.HEADING
+    if len(split_lines) > 0 and split_lines[0].startswith("```") and split_lines[-1].endswith("```"):
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in split_lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in split_lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    i = 1
+    if block.startswith(f"{i}. "):
+        for line in split_lines:
+            if not line.startswith("{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.ORDERED_LIST
+    
+    return BlockType.PARAGRAPH
+    
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -153,6 +153,114 @@ This is the same paragraph - on a new line?
             ]
         self.assertEqual(expected,blocks)
 
+class TestMarkdownBlockToBlockType(unittest.TestCase):
+    def test_block_to_block_type_paragraph(self):
+        block = "This is a paragraph block"
+        block_type = block_to_block_type(block)
+        expected = BlockType.PARAGRAPH
+        self.assertEqual(expected, block_type)
+
+    def test_block_to_block_type_heading(self):
+        blocks = ["#This is a heading block",
+                  "##This is a heading block",
+                  "###This is a heading block",
+                  "####This is a heading block",
+                  "#####This is a heading block",
+                  "######This is a heading block"
+                  ]
+        block_type0 = block_to_block_type(blocks[0])
+        block_type1 = block_to_block_type(blocks[1])
+        block_type2 = block_to_block_type(blocks[2])
+        block_type3 = block_to_block_type(blocks[3])
+        block_type4 = block_to_block_type(blocks[4])
+        block_type5 = block_to_block_type(blocks[5])
+        expected = BlockType.HEADING
+        self.assertEqual(expected, block_type0)
+        self.assertEqual(expected, block_type1)
+        self.assertEqual(expected, block_type2)
+        self.assertEqual(expected, block_type3)
+        self.assertEqual(expected, block_type4)
+        self.assertEqual(expected, block_type5)
+
+    def test_block_to_block_type_invalid_heading(self):
+        blocks = ["This is a heading block#",
+                  "This is a heading block\n## This is a heading block",
+                  ">###This is a heading block",
+                  "-####This is a heading block",
+                  "1#####This is a heading block",
+                  "This### is a heading block"
+                  ]
+        block_type0 = block_to_block_type(blocks[0])
+        block_type1 = block_to_block_type(blocks[1])
+        block_type2 = block_to_block_type(blocks[2])
+        block_type3 = block_to_block_type(blocks[3])
+        block_type4 = block_to_block_type(blocks[4])
+        block_type5 = block_to_block_type(blocks[5])
+        expected = BlockType.HEADING
+        self.assertNotEqual(expected, block_type0)
+        self.assertNotEqual(expected, block_type1)
+        self.assertNotEqual(expected, block_type2)
+        self.assertNotEqual(expected, block_type3)
+        self.assertNotEqual(expected, block_type4)
+        self.assertNotEqual(expected, block_type5)
+
+    def test_block_to_block_type_code(self):
+        block = "```This is a code block```"
+        block_type = block_to_block_type(block)
+        expected = BlockType.CODE
+        self.assertEqual(expected, block_type)
+
+    def test_block_to_block_type_invalid_code(self):
+        blocks = ["``This is a code block```",
+                  "`This is a code block```",
+                  "This is a code block```",
+                  "```This is a code block``",
+                  "```This is a code block`",
+                  "```This is a code block"
+                  ]
+        block_type0 = block_to_block_type(blocks[0])
+        block_type1 = block_to_block_type(blocks[1])
+        block_type2 = block_to_block_type(blocks[2])
+        block_type3 = block_to_block_type(blocks[3])
+        block_type4 = block_to_block_type(blocks[4])
+        block_type5 = block_to_block_type(blocks[5])
+        expected = BlockType.CODE
+        self.assertNotEqual(expected, block_type0)
+        self.assertNotEqual(expected, block_type1)
+        self.assertNotEqual(expected, block_type2)
+        self.assertNotEqual(expected, block_type3)
+        self.assertNotEqual(expected, block_type4)
+        self.assertNotEqual(expected, block_type5)
+
+    def test_block_to_block_type_quote(self):
+        block = ">This is a quote block"
+        block_type = block_to_block_type(block)
+        expected = BlockType.QUOTE
+        self.assertEqual(expected, block_type)
+
+    def test_block_to_block_type_invalid_quote(self):
+        block = "->This is a quote block"
+        block_type = block_to_block_type(block)
+        expected = BlockType.QUOTE
+        self.assertNotEqual(expected, block_type)
+
+    def test_block_to_block_type_ordered_list(self):
+        block = "1. This is a ordered list block\n2. This is an ordered list block\n3. This is an ordered list block\n4. This is an ordered list block"
+        block_type = block_to_block_type(block)
+        expected = BlockType.ORDERED_LIST
+        self.assertEqual(expected, block_type)
+
+    def test_block_to_block_type_ordered_list(self):
+        block = "1. This is a ordered list block\n2. This is an ordered list block\n2. This is an ordered list block\n4. This is an ordered list block"
+        block_type = block_to_block_type(block)
+        expected = BlockType.ORDERED_LIST
+        self.assertNotEqual(expected, block_type)
+
+    def test_block_to_block_type_unordered_list(self):
+        block = "- This is a unordered lsit block\n- This is a unordered lsit block\n>- This is a unordered lsit block\n- This is a unordered lsit block\n"
+        block_type = block_to_block_type(block)
+        expected = BlockType.UNORDERED_LIST
+        self.assertNotEqual(expected, block_type)
     
 if __name__ == "__main__":
     unittest.main()
