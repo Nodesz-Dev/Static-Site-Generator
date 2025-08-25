6fab4c11e362db3a1967428abb3c15d33ae70fc4
Nodesz-Dev <harleykempita@gmail.com>
2025-08-25 16:20:49 +1000

    created the copysourcefiles() function in the main.py file and added in the accompanying files to go with it. function was tested manually

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -97,13 +97,4 @@ def text_to_textnodes(text):
 
     return nodes
 
-def markdown_to_blocks(markdown):
-    split_blocks = markdown.split("\n\n")
-    full_blocks = []
-    for block in split_blocks:
-        if block == "":
-            continue
-        stripped_block = block.strip()
-        full_blocks.append(stripped_block)
 
-    return full_blocks
--- src/main.py
+++ src/main.py
@@ -1,9 +1,47 @@
-from textnode import *
+import os
+import shutil
 
+PUBLIC_DIRECTORY = "/home/brain/workspace/github.com/Static-Site-Generator/public"
+STATIC_DIRECTORY = "/home/brain/workspace/github.com/Static-Site-Generator/static"
 
 def main():
-    Test_Node = TextNode("Dummy Text", TextType.BOLD, "Dummy URL")
+    CopySourceFiles(STATIC_DIRECTORY, PUBLIC_DIRECTORY, True)
+    print("done")
 
-    print(Test_Node)
+def CopySourceFiles(src, dst, cleardir=False):
+    if not os.path.exists(src):
+        raise ValueError(f"Error: {src} file location does not exist")
+    
+    if cleardir:
+        if os.path.exists(dst):
+            shutil.rmtree(dst)
+        os.mkdir(dst)
+        pass
+
+    file_list = os.listdir(src)
+    for file in file_list:
+        full_path = os.path.join(src,file)
+        if os.path.isfile(full_path):
+            shutil.copy(full_path, dst)
+        elif os.path.isdir(full_path):
+            dirpath = os.path.join(dst, file)
+            os.mkdir(dirpath)
+            CopySourceFiles(os.path.join(src, file), dirpath)
+        else:
+            raise Exception(f"Error: Something went wrong with file at path {file}")
+
+def extract_title(markdown):
+    lines = markdown.split("\n")
+
+    for line in lines:
+        if line.startswith("#") and not line.startswith("##"):
+            stripped_line = line.strip("# ")
+            print(stripped_line)
+            return stripped_line
+    
+    raise Exception("Error: No Header found")
+
+def generate_page():
+    pass
 
 main()
